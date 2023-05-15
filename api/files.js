324461f7826948ffe7567bbb66f7cdafd1b941ad
Michael McKain <38301677+mikemckain@users.noreply.github.com>
2023-05-15 00:01:06 -0400

    specific CORs allowal for supermirage.pics

--- api/files.js
+++ api/files.js
@@ -1,40 +1,43 @@
 const axios = require("axios");
 const cors = require("cors");
 
-module.exports = cors(async function (req, res) {
-  async function getB2Authorization() {
+module.exports = cors(
+  { origin: "https://supermirage.pics" },
+  async function (req, res) {
+    async function getB2Authorization() {
+      const response = await axios({
+        method: "get",
+        url: "https://api.backblazeb2.com/b2api/v2/b2_authorize_account",
+        auth: {
+          username: process.env.B2_USERNAME,
+          password: process.env.B2_PASSWORD,
+        },
+      });
+
+      return response.data;
+    }
+
+    const auth = await getB2Authorization();
+
     const response = await axios({
-      method: "get",
-      url: "https://api.backblazeb2.com/b2api/v2/b2_authorize_account",
-      auth: {
-        username: process.env.B2_USERNAME,
-        password: process.env.B2_PASSWORD,
+      method: "post",
+      url: `${auth.apiUrl}/b2api/v2/b2_list_file_names`,
+      headers: {
+        Authorization: auth.authorizationToken,
+      },
+      data: {
+        bucketId: process.env.B2_BUCKET_ID,
+        maxFileCount: 100, // Change this to the maximum number of files you want to fetch
       },
     });
 
-    return response.data;
+    const files = response.data.files.map((file) => {
+      return {
+        ...file,
+        url: `${auth.downloadUrl}/file/supermirage/${file.fileName}`,
+      };
+    });
+
+    res.json(files);
   }
-
-  const auth = await getB2Authorization();
-
-  const response = await axios({
-    method: "post",
-    url: `${auth.apiUrl}/b2api/v2/b2_list_file_names`,
-    headers: {
-      Authorization: auth.authorizationToken,
-    },
-    data: {
-      bucketId: process.env.B2_BUCKET_ID,
-      maxFileCount: 100, // Change this to the maximum number of files you want to fetch
-    },
-  });
-
-  const files = response.data.files.map((file) => {
-    return {
-      ...file,
-      url: `${auth.downloadUrl}/file/supermirage/${file.fileName}`,
-    };
-  });
-
-  res.json(files);
-});
+);
